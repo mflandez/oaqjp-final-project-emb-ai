6c9aeb561908c4beaebac116aaf436d95bad922d
mflandez <flandezflandez@gmail.com>
2025-03-17 13:16:09 -0400

    server.py is 10/10 score after pylint was used

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
 from flask import Flask, render_template, request
@@ -7,14 +11,16 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion detection")
 
 @app.route("/emotionDetector")
-def emotionDetector():
-
+def emo_detector():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion_detector over it using sentiment_analysis()
+        function. The output returned shows the label and its confidence 
+        score for the provided text.
+    '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the emotion detector function and store the response
     response = emotion_detector(text_to_analyze)
-    print(response)
-        
     # Extract the emotion scores (adjust keys based on the actual response structure)
     anger = response['anger']
     disgust = response['disgust']
@@ -37,7 +43,9 @@ def emotionDetector():
 
 @app.route("/")
 def render_index_page():
-
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
